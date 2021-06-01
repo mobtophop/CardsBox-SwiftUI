1bb303aa78f3518268ac290d1439f7eb9604a831
mobtophop <mobtophop@gmail.com>
2021-06-01 09:44:57 +0300

    Initial Commit

---
+++ CardsBox/CardsBoxApp.swift
@@ -0,0 +1,20 @@
+//
+//  CardsBoxApp.swift
+//  CardsBox
+//
+//  Created by Eugene Shapovalov on 01.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct CardsBoxApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
